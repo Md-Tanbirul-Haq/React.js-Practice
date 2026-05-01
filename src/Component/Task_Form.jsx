27dd9b94d8c729fe1b3fb12a8d9f7a3ac684bd09
Md-Tanbirul-Haq <tanbirul15-3940@diu.edu.bd>
2026-05-02 00:17:03 +0600

    5

--- src/Component/Task_Form.jsx
+++ src/Component/Task_Form.jsx
@@ -1,10 +1,10 @@
 "use client";
 
+import { Label, ListBox, Select } from "@heroui/react";
+import { Envelope } from "@gravity-ui/icons";
+import { Button, Input, Modal, Surface, TextField } from "@heroui/react";
 
-import {Envelope} from "@gravity-ui/icons";
-import {Button, Input, Label, Modal, Surface, TextField} from "@heroui/react";
-
-export function Task_Form({createPost}) {
+export function Task_Form({ createPost }) {
   return (
     <Modal>
       <Button variant="secondary">Add Task</Button>
@@ -35,18 +35,74 @@ export function Task_Form({createPost}) {
                     <Label>Details </Label>
                     <Input placeholder="Enter task Details " />
                   </TextField>
-                  
+                  <TextField className="w-full" name="details" type="text">
+
+                    <Select placeholder="Select one">
+                      <Label>Status</Label>
+                      <Select.Trigger>
+                        <Select.Value />
+                        <Select.Indicator />
+                      </Select.Trigger>
+                      <Select.Popover>
+                        <ListBox>
+                          <ListBox.Item id="florida" textValue="Florida">
+                            pending
+                            <ListBox.ItemIndicator />
+                          </ListBox.Item>
+                          <ListBox.Item id="delaware" textValue="Delaware">
+                            in progress
+                            <ListBox.ItemIndicator />
+                          </ListBox.Item>
+                          <ListBox.Item id="california" textValue="California">
+                            completed
+                            <ListBox.ItemIndicator />
+                          </ListBox.Item>
+                        </ListBox>
+                      </Select.Popover>
+                    </Select>
+
+
+
+
+                  </TextField>
+                  <TextField className="w-full" name="details" type="text">
+                     <Select placeholder="Select one">
+                      <Label>Priority</Label>
+                      <Select.Trigger>
+                        <Select.Value />
+                        <Select.Indicator />
+                      </Select.Trigger>
+                      <Select.Popover>
+                        <ListBox>
+                          <ListBox.Item id="florida" textValue="Florida">
+                            high
+                            <ListBox.ItemIndicator />
+                          </ListBox.Item>
+                          <ListBox.Item id="delaware" textValue="Delaware">
+                            medium
+                            <ListBox.ItemIndicator />
+                          </ListBox.Item>
+                          <ListBox.Item id="california" textValue="California">
+                            low
+                            <ListBox.ItemIndicator />
+                          </ListBox.Item>
+                        </ListBox>
+                      </Select.Popover>
+                    </Select>
+                  </TextField>
+
+
                   <Modal.Footer>
-              <Button slot="close" variant="secondary">
-                Cancel
-              </Button>
-              {/* <Button slot="close">Submit</Button> */}
-              <Button type="submit" >Submit</Button>
-            </Modal.Footer>
+                    <Button slot="close" variant="secondary">
+                      Cancel
+                    </Button>
+                    {/* <Button slot="close">Submit</Button> */}
+                    <Button type="submit" >Submit</Button>
+                  </Modal.Footer>
                 </form>
               </Surface>
             </Modal.Body>
-            
+
           </Modal.Dialog>
         </Modal.Container>
       </Modal.Backdrop>
